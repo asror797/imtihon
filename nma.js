69163284e3773c80aed87071c1625549821ece9f
asror797 <asror797shahobiddinov@gmail.com>
2021-11-20 14:21:21 +0500

    second deploy

---
+++ nma.js
@@ -0,0 +1,50 @@
+// var app = document.querySelector("#app")
+// var btn = document.createElement("button");
+//     btn.classList.add("btn")
+//     btn.textContent="like";
+//     app.appendChild(btn)
+
+// var btn = document.createElement("button");
+//     btn.classList.add("btn")
+//     btn.textContent="like";
+//     app.appendChild(btn)
+//     btn.setAttribute("onclick","my()")
+
+
+// var btn = document.createElement("button");
+//     btn.classList.add("btn")
+//     btn.textContent="like";
+//     app.appendChild(btn)
+//     btn.setAttribute("onclick","my()")
+
+
+// var btn = document.createElement("button");
+//     btn.classList.add("btn")
+//     btn.textContent="like";
+//     app.appendChild(btn)
+//     btn.setAttribute("onclick","my()")
+// var txt = document.getElementById(txt)
+
+// var count = []
+// console.log(count);
+
+
+// function my(){
+//   if(btn.textContent=="like"){
+//     btn.textContent="dislike";
+//     count.push(1);
+//     console.log(count);
+//   }else {
+//     btn.textContent="like"
+//     count.pop()
+//     console.log(count);
+//   }
+// }
+
+// console.log(btn)
+
+
+
+let btn = document.createElement("p")
+
+console.log(btn);
